207666d80343b89957dcdb8391b9ba00c425dc5e
Rio-Sh <shrio.wr@gmail.com>
2022-02-14 19:31:54 +0900

    Setting Font #7

--- app/src/main/java/com/io/github/rio_sh/quickwordbook/ui/theme/Type.kt
+++ app/src/main/java/com/io/github/rio_sh/quickwordbook/ui/theme/Type.kt
@@ -1,11 +1,17 @@
 package com.io.github.rio_sh.quickwordbook.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.io.github.rio_sh.quickwordbook.R
+
 //Replace with your font locations
-val Roboto = FontFamily.Default
+private val Roboto = FontFamily(
+	Font(R.font.roboto_regular, FontWeight.W400),
+	Font(R.font.roboto_medium, FontWeight.Medium)
+)
 
 val AppTypography = Typography(
 	displayLarge = TextStyle(
