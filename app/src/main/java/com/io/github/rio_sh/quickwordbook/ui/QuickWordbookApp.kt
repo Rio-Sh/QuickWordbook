f88dcf521bb2101831fbe49af78b85d7fc77c7ab
Rio-Sh <shrio.wr@gmail.com>
2022-03-29 22:35:57 +0900

    Replace deprecate methods and Adjust Screen padding for landscape #36

--- app/src/main/java/com/io/github/rio_sh/quickwordbook/ui/QuickWordbookApp.kt
+++ app/src/main/java/com/io/github/rio_sh/quickwordbook/ui/QuickWordbookApp.kt
@@ -1,19 +1,15 @@
 package com.io.github.rio_sh.quickwordbook.ui
 
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.statusBarsPadding
 import com.io.github.rio_sh.quickwordbook.ui.theme.QuickWordbookTheme
 
 @Composable
 fun QuickWordbookApp() {
     QuickWordbookTheme {
-        ProvideWindowInsets {
-            val navController = rememberNavController()
-            NavHost(navController = navController, modifier = Modifier.statusBarsPadding())
-        }
+        val navController = rememberNavController()
+        NavHost(navController = navController, modifier = Modifier.statusBarsPadding())
     }
 }
